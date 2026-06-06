30f8b084a40dcd55b41d5df9f7f8cbb35b05d267
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-06 16:59:37 +0530

    feat: Implement AI response generation with new AI controller and routes

--- tiny-cats/backend/src/app.ts
+++ tiny-cats/backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import CatRouter from './routes/cat.routes'
+import AiRouter from './routes/ai.routes';
 const app = express();
 
 app.use(express.json());
 app.use("/api/cats", CatRouter);
+app.use("/api/ai", AiRouter);
 // app.get('/', (req: express.Request, res: express.Response) => {
 //   res.send({
 //     success: true,
--- tiny-cats/backend/src/services/gemini.service.ts
+++ tiny-cats/backend/src/services/gemini.service.ts
@@ -1,13 +1,14 @@
-import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-
-const ai = new GoogleGenAI({
-  apiKey: process.env.GEMINI_API_KEY!,
-});
-
 export async function generateAiResponse(prompt: string) {
+  const { GoogleGenAI } = await import("@google/genai");
+
+  const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY!,
+  });
+
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: prompt,
