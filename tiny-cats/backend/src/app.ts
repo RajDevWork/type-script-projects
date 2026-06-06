2dfc58f8382d8f09d9298b0ce9d8a86b8692630b
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-06 16:34:08 +0530

    feat: Update server port to 5000 and configure Cat router in app

--- tiny-cats/backend/src/app.ts
+++ tiny-cats/backend/src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-
+import CatRouter from './routes/cat.routes'
 const app = express();
 
 app.use(express.json());
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send({
-    success: true,
-    message: 'Welcome to the Tiny Cats API!',
-  });
-});
+app.use("/api/cats", CatRouter);
+// app.get('/', (req: express.Request, res: express.Response) => {
+//   res.send({
+//     success: true,
+//     message: 'Welcome to the Tiny Cats API!',
+//   });
+// });
 
 export default app;
--- tiny-cats/backend/src/server.ts
+++ tiny-cats/backend/src/server.ts
@@ -4,7 +4,7 @@ import app from './app'
 import connectDB from './config/db';
 
 connectDB();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
