b1b9812c25676e72e10e5a2c9fea30c6e0843fa4
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-06 15:38:36 +0530

    feat: Add JSON parsing middleware to Express app

--- tiny-cats/backend/src/app.ts
+++ tiny-cats/backend/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-
+app.use(express.json());
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send({
     success: true,
