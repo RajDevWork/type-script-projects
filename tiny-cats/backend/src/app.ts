ef7237d0eb6266cfb91a589c3fb77b54c47efa25
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-06 17:04:01 +0530

    feat: Add AI recommendation feature with controller, service, and routes

--- tiny-cats/backend/src/app.ts
+++ tiny-cats/backend/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import CatRouter from './routes/cat.routes'
 import AiRouter from './routes/ai.routes';
+import aiRecommendRoutes from './routes/aiRecommend.routes';
 const app = express();
 
 app.use(express.json());
 app.use("/api/cats", CatRouter);
 app.use("/api/ai", AiRouter);
+app.use("/api/aiRecommend", aiRecommendRoutes);
+
 // app.get('/', (req: express.Request, res: express.Response) => {
 //   res.send({
 //     success: true,
