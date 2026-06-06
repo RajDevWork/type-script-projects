3b6bca362ae4359a42e8c730de6dd2f5a702224c
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-06 15:18:57 +0530

    feat: Implement database connection setup in server and add connectDB function

--- tiny-cats/backend/src/server.ts
+++ tiny-cats/backend/src/server.ts
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app'
+import connectDB from './config/db';
 
+connectDB();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
