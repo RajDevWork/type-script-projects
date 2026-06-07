1a00fddfe96bee30c939c5084522ec16b96c7180
Rajeshwar Parihar <rajparihar14397@gmail.com>
2026-06-07 16:57:03 +0530

    feat: register recommend_cats tool for cat breed recommendations

--- tiny-cats/mcp_server/src/index.ts
+++ tiny-cats/mcp_server/src/index.ts
@@ -1,6 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
+import { recommendCatsTool } from "./tools/recommendCats.tool.ts";
 
 
 // Create server instance
@@ -8,3 +9,24 @@ const server = new McpServer({
   name: "tiny-cats",
   version: "1.0.0",
 });
+
+//register a tool
+server.registerTool("recommend_cats",{
+    title:"recommend_cats",
+    description:"Recommend a best cat breed according to inputs",
+    inputSchema:{
+        kidsFriendly:z.boolean(),
+        apartmentFriendly:z.boolean()
+    }
+}, async ({kidsFriendly,apartmentFriendly})=>{
+    const result = await recommendCatsTool(kidsFriendly,apartmentFriendly)
+    
+    return {
+        content:[
+            {
+                type:'text',
+                text:JSON.stringify(result)
+            }
+        ]
+    }
+})
--- tiny-cats/mcp_server/src/tools/recommendCats.tool.ts
+++ tiny-cats/mcp_server/src/tools/recommendCats.tool.ts
@@ -1,5 +1,7 @@
 import axios from "axios"
 
+
+//create a tool for mcp calling
 export const recommendCatsTool = async(kidsFriendly:boolean,apartmentFriendly:boolean) => {
     const res = await axios.post("http://localhost:5000/api/cats/recommend",{
         kidsFriendly,
